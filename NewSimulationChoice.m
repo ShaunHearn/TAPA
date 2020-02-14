abe1758e7773259397d1bb64eccf9f715fb5e70e
Shaun Hearn <shaunhearn@cmail.carleton.ca>
2020-02-14 12:51:23 -0500

    changed bounds

--- NewSimulationChoice.m
+++ NewSimulationChoice.m
@@ -11,7 +11,7 @@ xm = x(1:nx-1) + 0.5*dx;
 
 %Nd = 1e16*1e6;
 %Nd = linspace(1e16,20e16,nx); % Const. 1/cm3 (100 cm/m)^3
-NdRange = linspace(1,3,nx);
+NdRange = linspace(36,40,nx);
 Nd = exp(NdRange);
 NetDoping = ones(1,nx).*Nd; % doping
 
